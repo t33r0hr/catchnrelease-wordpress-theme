179d8bf903e108e8e62e456af51c4f5564c26162
Matthias Buchholz <mb@averagejoe.de>
2018-05-14 21:25:01 +0200

    footer widgets with social buttons

--- template-parts/footer/footer-widgets.php
+++ template-parts/footer/footer-widgets.php
@@ -21,13 +21,13 @@ if ( is_active_sidebar( 'sidebar-2' ) ) :
 				<?php dynamic_sidebar( 'sidebar-2' ); ?>
 			</div>
 		<?php }
-		/* 
+		 
 		if ( is_active_sidebar( 'sidebar-3' ) ) { ?>
 			<div class="widget-column footer-widget-2">
 				<?php dynamic_sidebar( 'sidebar-3' ); ?>
 			</div>
 		<?php } 
-		*/ ?>
+		 ?>
 	</aside><!-- .widget-area -->
 
 <?php endif; ?>
